1d164cdf31dc3501f50af9024bef88a72a67a9d9
Ryan Zoerb <ryan.zoerb@gmail.com>
2023-04-20 22:42:34 -0500

    Add biases and non-linearity

--- common.py
+++ common.py
@@ -11,21 +11,30 @@ input = np.array([
     [-1.0, -1.0], [0.0, -1.0], [1.0, -1.0],
 ])
 
-expected = np.array([
-     0,  1,  1,
-    -1,  0,  1,
-    -1, -1,  0,
+expected1 = np.array([
+    1, 1, 1,
+    1, 1, 0,
+    1, 0, 0,
+]).reshape(-1, 1) # Convert from row vector to column vector
+
+expected2 = np.array([
+    0, 1, 1,
+    1, 0, 0,
+    1, 0, 0,
 ]).reshape(-1, 1) # Convert from row vector to column vector
 
 
 # Matrix of data for testing
-vals = np.linspace(-1.0, 1.0, 32)
+vals = np.linspace(-1.1, 1.1, 32)
 test = np.array([[x, -y] for y in vals for x in vals])
 
 
 # For each row pair in (row[i](a), row[i](b)), perform an outer product
 def elemOuterF(a, b): return np.outer(a, b)
 elemOuter = np.vectorize(elemOuterF, signature='(m),(n)->(m,n)')
+
+def elemDotF(a, b): return a * b
+elemDot = np.vectorize(elemDotF, signature='(m, n),(o)->(m, n)')
 
 
 cmap1 = ListedColormap(
@@ -40,14 +49,16 @@ cmap = cmap2
 def plotScatter(coords, data):
     plt.scatter(coords[:, 0], coords[:, 1], c=data, cmap=cmap)
 
-def plot(data):
+def plot(data, expected):
     fig, ax = plt.subplots()
 
     nData = round(sqrt(len(data)))
     ax.imshow(
         data.reshape(nData, nData),
         cmap=cmap,
-        extent=[-1.0, 1.0, -1.0, 1.0],
-        norm=CenteredNorm(halfrange=0.5)
+        extent=[-1.1, 1.1, -1.1, 1.1],
+        norm=CenteredNorm(vcenter=0.5, halfrange=0.2)
     )
+    ax.scatter(input[:, 0], input[:, 1], c=expected, cmap=cmap)
+
     plt.show()
